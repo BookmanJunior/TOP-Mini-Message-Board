7c33501fb237d87901652441ec1e84ca47beda7f
Azam Gulov <gulovazam@gmail.com>
2023-12-06 18:14:38 +0500

    Import commentController
    remove messages array
    remove old post route
    update index view loop

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,14 @@
 var express = require("express");
 var router = express.Router();
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+const commentController = require("../controller/commentController");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Mini Messageboard", messages: messages });
-});
+router.get("/", commentController.get_comments);
 
 router.get("/new", function (req, res, next) {
   res.render("form");
 });
 
-router.post("/new", function (req, res, next) {
-  const username = req.body.username;
-  const message = req.body.userMessage;
-  messages.push({ text: message, user: username, added: new Date() });
-  res.redirect("/");
-});
+router.post("/new");
 
 module.exports = router;
