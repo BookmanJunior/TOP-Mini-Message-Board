c66b12830c384168685357f49541d91486bfd1a7
Azam Gulov <gulovazam@gmail.com>
2023-12-06 18:11:27 +0500

    Add schema method to format date
    fix user ref

--- model/comment.js
+++ model/comment.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const User = require("./user");
+const { format } = require("date-fns");
 
 const commentSchema = new mongoose.Schema({
   comment: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: Schema.Types.ObjectId, ref: User, required: true },
   date: { type: Date, required: true },
+});
+
+commentSchema.virtual("sorted_date").get(function () {
+  return format(this.date, "MMM do, h:m:a");
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
