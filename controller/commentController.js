d62982ec411d362c6c88727a230507998e783065
Azam Gulov <gulovazam@gmail.com>
2023-12-07 18:05:43 +0500

    Add post comment function
    add validation
    fix ejs not rendering comments correctly

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const Comment = require("../model/comment");
+const User = require("../model/user");
 
 // Get comments
 exports.get_comments = asyncHandler(async (req, res, next) => {
@@ -9,8 +10,46 @@ exports.get_comments = asyncHandler(async (req, res, next) => {
     .populate("user")
     .exec();
 
-  res.render("index", { title: "Mini Messageboard", comments });
+  res.render("index", {
+    title: "Mini Messageboard",
+    comments,
+  });
 });
 
 // Post comment
-exports.post_comment = [];
+exports.post_comment = [
+  body("username", "Username can't be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("userMessage", "Comment can't be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const error = validationResult(req);
+
+    const new_user = new User({ name: req.body.username });
+
+    const comments = {
+      comment: req.body.userMessage,
+      user: new_user,
+      date: new Date(),
+    };
+
+    // check for error
+    if (!error.isEmpty()) {
+      //error. resend form
+
+      res.render("form", {
+        title: "Mini Messageboard",
+        comments,
+        errors: error.array(),
+      });
+    } else {
+      await Promise.all([new Comment(comments).save(), new_user.save()]);
+      res.redirect("/");
+    }
+  }),
+];
